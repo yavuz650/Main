8a7781a97ae07ee637419da79c8f20e2a456aea6
Yavuz Selim <0yavuz0@gmail.com>
2017-06-16 11:14:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,10 @@ int main()
 	start_color();			/* Start the color functionality */
 	cbreak();			/* Line buffering disabled, Pass on
 					 * everty thing to me 		*/
-	keypad(stdscr, TRUE);		/* I need that nifty F1 	*/
+	keypad(stdscr, TRUE);		
 	noecho();
 	init_pair(1, COLOR_CYAN, COLOR_BLACK);
 
-	/* Initialize the window parameters */
 	attron(COLOR_PAIR(1));
 	refresh();
 	curs_set(0);
